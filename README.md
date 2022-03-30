ddc2ea7a6e2df83fa4b86a75d19d7cdb214388d7
Bigfootdude <bigfootdude2377@gmail.com>
2022-03-30 09:23:20 -0400

    Edit README.md and index.html and script.js and style.css

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # RPSGame
-A simple Rock Paper Scissors game
+This game is little more than a practice rps game with a mode of rock paper scissors lizard spock.
+
+The mode of rpsls was the hardest to set up, if a more elegent solution than a true false variable at the top and
+a series of if then statements at every important place in the program comes to my attention in the future I will
+then probably be compeled to implement it and update my repos.  Until then, I release this program for your ammusement.
+
+The basic rps strategy was to convert rock to the number zero, paper to one and scissors to two.  Then a simple math comparison
+could determine victory or defeat which would cut down on if then statements in the program.  Rpsls mode involves using more
+numbers and a more complicated system that either subtracts or adds the player number to one end or the other of the 0-4 number
+line.  Then comparison of the numbers becomes simple, or at least easier.  This method seems to require a seperate comparison if 
+player is smaller or larger than computer.  If they are equal then it's just a tie.
+
+The mode button is how you change the appearance of the page between the games and reset the score.  It hides the spock and lizard
+buttons on normal mode and reveals them on the other mode.
+
+Most of the variables on the top of the page are to initialize the program properly.  Again if a better way reveals itself to me
+I will probably be compelled to return to this game and refactor it.
+
+And finally, it is all called by the buttons on the page.  Each one starting a new round in the game and changing the global variables
+in the game logic.
--- script.js
+++ script.js
@@ -1,84 +1,150 @@
-function produceText(n) {
-	// produce text from numbers
-	if (n == 1) {
-		return "rock";
-	} else if (n == 2) {
-		return "paper";
-	} else if (n == 3) {
-		return "scissors";
-	}
-}
+let pC = null;
+let cC = null;
+let normalGame = true;
 
-function computerPlay() {
-	// make random number
-	let answer = Math.floor(Math.random() * 3) + 1;
-	answer = produceText(answer);
-	return answer;
-}
+const score = [0,0];
 
-function playerPlay() {
-	// prompt and filter player choice.
-	let answer = prompt("Rock paper or scissors? ");
-	answer = answer.toLowerCase();
-	return answer;
-}
+spock.style.display = 'none';// these establish a normal view
+lizard.style.display = 'none';
 
+function toggleMode(n) {// this toggles the game type.
 
-function playRound(p, c) {
-	// compare player vs computer for victory.
-	if (p == "rock") {
-		if (c == "rock") {
-			return "Tie";
-		} else if (c == "paper") {
-			return "Loose";
-		} else if (c == "scissors") {
-			return "Win";
-		}
-	} else if (p == "paper") {
-		if (c == "rock") {
-			return "Win";
-		} else if (c == "paper") {
-			return "Tie";
-		} else if (c == "scissors") {
-			return "Loose";
-		}
-	} else if (p == "scissors") {
-		if (c == "rock") {
-			return "Loose";
-		} else if (c == "paper") {
-			return "Win";
-		} else if (c == "scissors") {
-			return "Tie";
-		}
-	}
+    if (n == true) {
+        normalGame = false;         
+        spock.style.display = '';
+        lizard.style.display = '';
+        score[0] = 0;
+        score[1] = 0;
+        pC = null;
+        cC = null;
+        display('#victory', '');
+        display('#win', '');
+        display('#playerS', '');
+        display('#computerS', '');
+        display('#playerC', '');
+        display('#computerC', '');
+
+    } else if (n == false) {
+        normalGame = true;
+        spock.style.display = 'none';
+        lizard.style.display = 'none';        
+        score[0] = 0;
+        score[1] = 0;
+        pC = null;
+        cC = null;
+        display('#victory', '');
+        display('#win', '');
+        display('#playerS', '');
+        display('#computerS', '');
+        display('#playerC', '');
+        display('#computerC', '');
+
+    }
 }
 
 
-function game() {
-	// begin the game logic using above functions.
-	// goal is to have 5 turns, keep score, return victory.
-	let playerScore = 0;
-	let computerScore = 0;
-	for (let i = 0; i < 5; i++) {
-		const player = playerPlay();
-		const computer = computerPlay();
-		let results = playRound(player, computer);
-		if (results == "Win") {
-			playerScore++;
-            console.log(`You win, ${player} beats ${computer}!`);
-		} else if (results == "Tie") {
-			console.log(`You tie, you both chose ${computer}.`);
-		} else if (results == "Loose") {
-			computerScore++;
-			console.log(`You loose, ${computer} beats ${player}!`);
-		}}
-	if (playerScore < computerScore) {
-		return `You loose ${computerScore} to ${playerScore}.`;
-	} else if (computerScore < playerScore) {
-		return `You win ${playerScore} to ${computerScore}.`;
-	} else {
-		return `You tie at ${playerScore}.`;
-	}
+function display(id, string) {
+    const screen  = document.querySelector(id);
+    screen.innerText = string;
+}
+
+function testVictory(player, computer) {
+    if (normalGame == true) {
+        if ((computer == player - 1) || (computer == player + 2)) {
+            score[0]++;
+            return 'Player wins';
+        } else if ((player == computer - 1) || (player == computer + 2)) {
+            score[1]++;
+            return 'Computer wins';
+        } else {
+            return 'tie';
+        }
+    } else if (normalGame == false) {
+
+        if (player > computer) {
+            difference = 4 - player;
+            player = player + difference;
+            computer = computer + difference;
+            if ((computer == player - 1) || (computer == player - 3)) {
+                 score[0]++;
+                 return 'Player wins';
+            }
+            else {
+                score[1]++;
+                return 'Computer wins';
+            }
+        } else if (player < computer) {
+             computer = computer - player;
+             player = player - player;
+             if ((computer == player + 2) || (computer == player + 4)) {
+                 score[0]++;
+                 return 'Player wins';
+            } else {
+                score[1]++;
+                return 'Computer wins';
+            }
+        } else {
+            return 'tie';
+        }
+    }      
+     
 
 }
-console.log(game());
+
+function translate(n) {
+    if (normalGame == true) {
+        if (n == 0) {
+            return 'rock';
+        } else if (n == 1) {
+            return 'paper';
+        } else if (n == 2) {
+            return 'scissors';
+        } else {
+            return "ERROR";
+        }
+    }else if (normalGame == false) {
+        if (n == 0) {
+            return "rock";
+        } else if (n == 1) {
+            return "paper";
+        } else if (n == 2) {
+            return "scissors";
+        } else if (n == 3) {
+            return "spock";
+        } else if (n == 4) {
+            return "lizard";
+        } else {
+            return "ERROR";
+        }
+    }
+}
+
+function playRound(p) {
+    if ((score[1] == 0) && (score[0] == 0)) display('#victory', '');
+
+    if (normalGame == true) {
+        cC = Math.floor(Math.random() * 3);
+    } else if (normalGame == false) {
+        cC = Math.floor(Math.random() * 5);
+    }
+    
+    pC = p;
+
+    display('#win',testVictory(pC, cC));
+
+    if (score[0] == 5) {
+        display('#victory','Player victory');
+        score[0] = 0;
+        score[1] = 0;
+
+    } else if (score[1] == 5) {
+        display('#victory','Computer victory');
+        score[0] = 0;
+        score[1] = 0;
+    }
+
+    display('#playerC', `Player: ${translate(pC)}`);
+    display('#computerC', `Computer: ${translate(cC)}`);
+    display('#playerS',`Player score:${score[0]}`);
+    display('#computerS', `Computer score:${score[1]}`);
+}
